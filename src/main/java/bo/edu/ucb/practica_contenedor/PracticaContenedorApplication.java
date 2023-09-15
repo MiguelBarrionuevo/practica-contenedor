74603826d4facec8988e98aeb1000ff712b8110a
MiguelBarrionuevo <miguel.barrionuevo@ucb.edu.bo>
2023-09-14 20:26:03 -0400

    Primera Version

---
+++ src/main/java/bo/edu/ucb/practica_contenedor/PracticaContenedorApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.practica_contenedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaContenedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaContenedorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/edu/ucb/practica_contenedor/PracticaContenedorApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.practica_contenedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaContenedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
